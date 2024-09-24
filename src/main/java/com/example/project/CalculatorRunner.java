b4fff8ba24ceeec6631ce4f805724c05dbeda72a
Diyor52 <diyora2@nycstudents.net>
2024-09-24 16:18:45 +0000

    Calculator

--- src/main/java/com/example/project/CalculatorRunner.java
+++ src/main/java/com/example/project/CalculatorRunner.java
@@ -5,8 +5,23 @@ public class CalculatorRunner {
 
         //test your program here
         //1. create an instance of the calculator class
+        Calculator cal = new Calculator("Cal1");
 
         //2. call any methods of that class to test
+        double operation = cal.performOperation("+", 5,6);
+        System.out.println(operation);
+        
+        boolean trueFalse= cal.divisibleBy(5, 6);
+        System.out.println(trueFalse);
 
+        String coords = cal.coordinatePair(5, 6);
+        System.out.println(coords);
+        
+        int absValue = cal.absoluteValue(5, 6);
+        System.out.println(absValue);
+        
+        String information = cal.info();
+
+        
     }
 }
